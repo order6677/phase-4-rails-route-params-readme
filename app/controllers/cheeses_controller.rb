4e9ed89542431efe8c52dfc438f633f0d2262adb
awadh <awadh.hassan@student.moringaschool.com>
2022-12-10 12:32:57 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,14 @@
 class CheesesController < ApplicationController
-
   def index
+  def vio
     cheeses = Cheese.all
     render json: cheeses
+  end
+
+end
+  def viosha
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
   end
 
 end
